6a4d312d0c8de7114c51e548330520d06105a300
hazyfossa <hazyfossa@gmail.com>
2026-03-12 22:07:05 +0500

    Change definition conventions

--- src/define.rs
+++ src/define.rs
@@ -1,6 +1,6 @@
 #[macro_export]
 macro_rules! define_env {
-    ($vis:vis $name:ident ($repr:ty) = auto parse $key:expr) => {
+    ($vis:vis $name:ident ($repr:ty) = $key:expr) => {
         impl $crate::parse::EnvironmentParse<String> for $name
         {
             type Error = <$repr as std::str::FromStr>::Err;
@@ -14,24 +14,28 @@ macro_rules! define_env {
             }
         }
 
-        $crate::define_env!($vis $name ($repr) = $key);
+        $crate::define_env!(@impl $vis $name ($repr) = $key);
     };
 
-    ($vis:vis $name:ident ($repr:ty) = parse raw $key:expr) => {
+    ($vis:vis $name:ident ($repr:ty) = raw $key:expr) => {
         impl $crate::parse::EnvironmentParse<std::ffi::OsString> for $name {
-            type Error = <$repr as TryParse>::Error;
+            type Error = <$repr as TryFrom<std::ffi::OsString>>::Error;
 
             fn env_serialize(self) -> std::ffi::OsString { self.0.into() }
 
-            fn env_deserialize(raw: std::ffi::OsString) -> $crate::parse::Result<Self> {
+            fn env_deserialize(raw: std::ffi::OsString) -> Result<Self, Self::Error> {
                 Ok(Self(<$repr>::try_from(raw)?))
             }
         }
 
-        $crate::define_env!($vis $name ($repr) = $key);
+        $crate::define_env!(@impl $vis $name ($repr) = $key);
     };
 
-    ($vis:vis $name:ident ($repr:ty) = $key:expr) => {
+    ($vis:vis $name:ident ($repr:ty) = custom $key:expr) => {
+        $crate::define_env!(@impl $vis $name ($repr) = $key);
+    };
+
+    (@impl $vis:vis $name:ident ($repr:ty) = $key:expr) => {
         $vis struct $name($repr);
 
         impl $crate::EnvVar for $name {
